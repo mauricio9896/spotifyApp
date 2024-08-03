461f33910686db810ab758aadf5c99fcd41b01bb
Mauricio Buitrago <mauricio9896@hotmail.com>
2024-08-03 16:55:06 -0500

    play and pause songs, fixed player service

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -5,52 +5,59 @@ import { BehaviorSubject } from 'rxjs';
 import { Device } from '../models/devices.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlayerService {
-
-  private spotifyApi : SpotifyWebApi.SpotifyWebApiJs;
+  private spotifyApi: SpotifyWebApi.SpotifyWebApiJs;
   public idTrack$ = new BehaviorSubject<string>('');
 
-  constructor( private spotifyService : SpotifyService ) {
+  constructor(private spotifyService: SpotifyService) {
     this.spotifyApi = new SpotifyWebApi();
   }
 
-  getDataUser() {
+  async getDataUser() {
     this.spotifyApi.setAccessToken(this.spotifyService.token);
-    this.spotifyApi.getMe().then(
+    const user = await this.spotifyApi.getMe();
+    console.log('user :>> ', user);
+  }
+
+  async playSong(uri: string){
+    const position_ms = await this.getStateSong(uri);
+    const device_id = await this.getDevies();
+    this.spotifyApi.play({
+      device_id,
+      uris: [uri],
+      position_ms
+    });
+  }
+
+
+  pauseSong() {
+    this.spotifyApi.pause();
+  }
+
+  private async getDevies(): Promise<string>{
+    const { devices } = await this.spotifyApi.getMyDevices();
+    const device = devices.find((device: any) => device.name === 'Web_SDK_App');
+    return device?.id || ''
+  }
+
+  private async getStateSong(uri : string): Promise<number> {
+    const { progress_ms , item } = await this.spotifyApi.getMyCurrentPlaybackState();
+    if(progress_ms && uri ===  item?.uri){
+      return progress_ms;
+    }
+    return 0;
+  }
+
+  setVolumen(volumen: number = 100) {
+    this.spotifyApi.setVolume(volumen).then(
       function (data) {
-        console.log('Información del usuario', data);
+        console.log('Volumen actualizado...');
       },
       function (err) {
         console.error(err);
       }
     );
-  }
-
-  playSong(uri: string){
-    this.getDevies().then(({ devices })=>{
-      const { id } = devices.find((device: Device) => device.name === 'Web_SDK_App');
-      this.spotifyApi.play({
-        device_id: id,
-        uris: [uri],
-      });
-    });
-  }
-
-  pauseSong(){
-    this.spotifyApi.pause();
-  }
-
-  getDevies(): Promise<any> {
-    return this.spotifyApi.getMyDevices();
-  }
-
-  setVolumen(volumen : number = 100 ){
-    this.spotifyApi.setVolume(volumen).then(function(data) {
-      console.log('Volumen actualizado...');
-    }, function(err) {
-      console.error(err);
-    });
   }
 }
