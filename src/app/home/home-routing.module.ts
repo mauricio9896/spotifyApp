7d325e14f9b14205170486f1273d790849d7e041
Mauricio Buitrago <mauricio9896@hotmail.com>
2025-02-01 12:45:27 -0500

    create new route "library"

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SavedUserComponent } from './components/saved-user/saved-user.component';
 
 export const HomeRoutes: Routes = [
   {
@@ -10,6 +11,10 @@ export const HomeRoutes: Routes = [
         path: 'search',
         loadChildren: () =>
           import('../search/search.module').then((x) => x.SearchModule),
+      },
+      {
+        path: 'library',
+        component: SavedUserComponent
       },
       { path: '', redirectTo: 'search', pathMatch: 'full' },
     ],
